d0c6d238365208d939934ab9ba97bfc2d334314d
Daniel Vieira <daniel01@gmx.com>
2023-05-21 20:30:42 -0300

    feat: make dataload idempotent by recreating the target table

--- helpers/clickhouse_operations.py
+++ helpers/clickhouse_operations.py
@@ -1,4 +1,5 @@
 import clickhouse_connect
+import logging
 
 class ClickHouseOperations:
 
@@ -14,5 +15,19 @@ class ClickHouseOperations:
         '''
         self.client.command(query)
 
+    def drop_table_if_exists(self, table_name):
+        self.client.command(f'DROP TABLE IF EXISTS {table_name}')
+
+    def insert_data(self, table_name, data, column_names):
+        self.client.insert(table_name, data, column_names)
+
     def insert_data(self, table_name, data, column_names):
         self.client.insert(table_name, data, column_names=column_names)
+
+    def check_if_table_exists(self, table_name):
+        try:
+            result = self.client.command(f"SHOW TABLES LIKE '{table_name}'")
+            return len(result) > 0
+        except Exception as e:
+            logging.error(f"An error occurred when checking if the table exists: {e}")
+            return False
--- load_data.py
+++ load_data.py
@@ -1,13 +1,22 @@
 import json
 import pandas as pd
+import logging
 from helpers.clickhouse_operations import ClickHouseOperations
 from helpers import config
 
+def setup_logging():
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
 def load_data(zipcodes_datafile_geojson):
+    logging.info('Loading data from JSON file.')
     with open(zipcodes_datafile_geojson) as file:
         data = json.load(file)
 
     rows = []
+    logging.info('Parsing features from JSON data.')
     for feature in data["features"]:
         row = {
             "zip": feature["properties"]["zip"],
@@ -17,21 +26,30 @@ def load_data(zipcodes_datafile_geojson):
         rows.append(row)
 
     df = pd.DataFrame(rows)
+    logging.info('Dataframe created.')
+
+    table_name = 'zip_codes'
 
     fields = {
         'zip': 'String',
         'type': 'String',
         'coordinates': 'String'
     }
+
+    logging.info('Setting up ClickHouse operations.')
     db = ClickHouseOperations(config.DB_HOST, config.DB_USERNAME, config.DB_PASSWORD)
-    db.create_table(table_name='zip_codes', fields=fields, order_by='zip')
 
     # Prepare data for insertion
+    logging.info('Preparing data for insertion.')
     data = [list(row) for row in df.itertuples(index=False, name=None)]
 
-    db.insert_data('zip_codes', data, ['zip', 'type', 'coordinates'])
-
+    logging.info('Inserting data.')
+    db.drop_table_if_exists(table_name)
+    db.create_table(table_name=table_name, fields=fields, order_by='zip')
+    db.insert_data(table_name=table_name, data=data, column_names=fields.keys())
+    logging.info('Data inserted successfully.')
 
 if __name__ == "__main__":
+    setup_logging()
     zipcodes_datafile_geojson = '/home/dvieira/tmp/easyhealth/eh-analytics/src/2023-05-19_01.58.58-usa_zip_codes_v7.geo.json'
     load_data(zipcodes_datafile_geojson)
